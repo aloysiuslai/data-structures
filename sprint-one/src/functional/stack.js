be92038d358e367f781f0d6bec0db60336a57a2e
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 12:49:12 -0700

    Pass stack (functional)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    var result = storage[count]
+    delete storage[count];
+    count--;
+
+    return result;
   };
 
   someInstance.size = function() {
+    if (count < 0){
+      return 0;
+    } else {
+      return count;
+    }
   };
 
   return someInstance;
