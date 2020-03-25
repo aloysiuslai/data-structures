2db9f0cbba9ddb65da3e587a001f77e4f4b01ae6
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 15:34:30 -0700

    Pass queue (functional-shared)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = {
+    storage: {},
+    count: 0,
+    first: 0,
+    last: 0,
+  }
+  _.extend(object, queueMethods);
+  return object;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(value){
+    this.count++;
+    this.storage[this.last] = value;
+    this.last++;
+  },
+  dequeue: function(){
+    var result = this.storage[this.first];
+    delete this.storage[this.first];
+    this.count--;
+    this.first++;
+    return result;
+  },
+  size: function(){
+    return this.count > 0 ? this.count : 0;
+  }
+};
