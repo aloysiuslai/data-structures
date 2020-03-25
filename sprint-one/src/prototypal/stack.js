3538e10f83c0ef06494bcdee7223e7bd2cafe67c
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 16:29:36 -0700

    Pass stack (prototypal)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = Object.create(stackMethods);
+  return object;
 };
 
-var stackMethods = {};
+// var stackMethods = {};
+var stackMethods = {
+  storage: {},
+  count: 0,
+  push: function(value){
+    this.count++;
+    this.storage[this.count] = value;
+  },
+
+  pop: function(){
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return result;
+  },
+
+  size: function(){
+    return this.count > 0 ? this.count : 0;
+  },
+};
 
 
