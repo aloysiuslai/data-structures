aa281b334179f704da54a9ccf7f1c6341a732aea
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 13:09:10 -0700

    Pass queue (functional)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[last] = value;
+    last++
   };
 
   someInstance.dequeue = function() {
+    var result = storage[first];
+    delete storage[first];
+    count--;
+    first++;
+    return result;
   };
 
   someInstance.size = function() {
+    if (count < 0){
+      return 0;
+    } else {
+      return count;
+    }
+
   };
 
   return someInstance;
