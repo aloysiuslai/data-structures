520648268f770dedf16297c7b67b295cbeb44193
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 15:02:27 -0700

    Pass stack (functional-shared)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = {
+    storage: {},
+    count: 0
+  }
+
+  _.extend(object, stackMethods);
+  return object;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.count++;
+    this.storage[this.count] = value;
+  },
 
+  pop: function(){
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return result;
+  },
 
+  size: function(){
+    return this.count > 0 ? this.count : 0;
+  },
+};
