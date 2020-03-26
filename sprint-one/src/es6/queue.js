3b664794337bc09e820410ca4ed47320898dd02b
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-26 00:46:56 -0700

    Pass queue (es6)

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.first = 0;
+    this.last = 0;
   }
 
+  enqueue(value) {
+    this.count++;
+    this.storage[this.last] = value;
+    this.last++;
+  }
+
+  dequeue() {
+    var result = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    this.count--;
+    return result;
+  }
+
+  size() {
+    return this.count > 0 ? this.count : 0;
+  }
 }
