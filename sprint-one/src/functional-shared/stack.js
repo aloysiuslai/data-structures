bddd8012869b8c306637fee94737d608e5f40826
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 17:08:44 -0700

    Pass stack (functional-shared) - Update Pomander warning

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -4,26 +4,26 @@ var Stack = function() {
   var object = {
     storage: {},
     count: 0
-  }
+  };
 
   _.extend(object, stackMethods);
   return object;
 };
 
 var stackMethods = {
-  push: function(value){
+  push: function(value) {
     this.count++;
     this.storage[this.count] = value;
   },
 
-  pop: function(){
+  pop: function() {
     var result = this.storage[this.count];
     delete this.storage[this.count];
     this.count--;
     return result;
   },
 
-  size: function(){
+  size: function() {
     return this.count > 0 ? this.count : 0;
   },
 };
