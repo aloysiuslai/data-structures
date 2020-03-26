31563176883dcf2cffdc6719f40a1b2469814029
aloysiuslai <aloysiuslai13@gmail.com>
2020-03-25 18:01:43 -0700

    Pass queue (prototypal)

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = Object.create(queueMethods);
+  return object;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  storage: {},
+  count: 0,
+  first: 0,
+  last: 0,
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.last] = value;
+    this.last++;
+  },
+  dequeue: function() {
+    var result = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    this.count--;
+
+    return result;
+  },
+  size: function() {
+    return this.count > 0 ? this.count : 0;
+  }
+};
 
 
